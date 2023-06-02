3b601c7c1bc941246b7b5e37634b86990b95693f
Dusan Odalovic <dusan.odalovic@gransoftware.de>
2023-06-02 11:25:11 +0200

    OB-4895: Improvements

--- src/main/java/com/vi/migrationtool/tenantservice/ImportDiocesesFromAgencyToTenantServiceTask.java
+++ src/main/java/com/vi/migrationtool/tenantservice/ImportDiocesesFromAgencyToTenantServiceTask.java
@@ -2,6 +2,7 @@ package com.vi.migrationtool.tenantservice;
 
 import com.vi.migrationtool.common.MigrationTasks;
 import com.vi.migrationtool.config.BeanAwareSpringLiquibase;
+import java.util.List;
 import liquibase.database.Database;
 import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
@@ -26,39 +27,46 @@ public class ImportDiocesesFromAgencyToTenantServiceTask extends MigrationTasks 
     val agencyJdbcTemplate =
         BeanAwareSpringLiquibase.getNamedBean("agencyServiceJdbcTemplate", JdbcTemplate.class);
 
-    var dioceseTableExist = doesTableExist(agencyJdbcTemplate, "agencyservice");
+    var dioceseTableExist = doesDioceseTableExist(agencyJdbcTemplate, "agencyservice");
     if (!dioceseTableExist) {
       log.info("Diocese table does not exist in agency service, skipping import");
       return;
     }
-    dioceseTableExist = doesTableExist(agencyJdbcTemplate, "tenantservice");
+    dioceseTableExist = doesDioceseTableExist(agencyJdbcTemplate, "tenantservice");
     if (!dioceseTableExist) {
       log.info("Diocese table does not exist in tenant service, skipping import");
       return;
     }
 
-    val dioceses =
-        agencyJdbcTemplate.query(
-            "select name from " + dioceseTableName,
-            (rs, rowNum) -> new Diocese(rs.getString("name")));
+    val dioceses = getAllDiocesesFromAgencyDB(agencyJdbcTemplate);
 
     log.info("The following dioceses will be migrated: {}", dioceses);
 
     var tenantServiceJdbcTemplate =
         BeanAwareSpringLiquibase.getNamedBean("tenantServiceJdbcTemplate", JdbcTemplate.class);
 
-    val batchUpdateResult =
-        tenantServiceJdbcTemplate.batchUpdate(
-            "insert ignore into " + dioceseTableName + " (name) values (?)",
-            dioceses,
-            1000,
-            (ps, diocese) -> ps.setString(1, diocese.name));
+    val batchResult = insertMissingDiocesesIntoTenantDB(dioceses, tenantServiceJdbcTemplate);
 
-    log.info("Inserted {} rows into {} table", batchUpdateResult.length, dioceseTableName);
+    log.info("Inserted {} rows into {} table", batchResult.length, dioceseTableName);
     log.info("Finished import of dioceses from agency to tenant");
   }
 
-  private static boolean doesTableExist(JdbcTemplate agencyJdbcTemplate, String tableSchema) {
+  private static int[][] insertMissingDiocesesIntoTenantDB(
+      List<Diocese> dioceses, JdbcTemplate tenantServiceJdbcTemplate) {
+    return tenantServiceJdbcTemplate.batchUpdate(
+        "insert ignore into " + dioceseTableName + " (name) values (?)",
+        dioceses,
+        1000,
+        (ps, diocese) -> ps.setString(1, diocese.name));
+  }
+
+  private static List<Diocese> getAllDiocesesFromAgencyDB(JdbcTemplate agencyJdbcTemplate) {
+    return agencyJdbcTemplate.query(
+        "select name from " + dioceseTableName, (rs, rowNum) -> new Diocese(rs.getString("name")));
+  }
+
+  private static boolean doesDioceseTableExist(
+      JdbcTemplate agencyJdbcTemplate, String tableSchema) {
     val dioceseTableExistsQuery =
         "select count(*) from information_schema.tables where table_name = ? and table_schema = '"
             + tableSchema
