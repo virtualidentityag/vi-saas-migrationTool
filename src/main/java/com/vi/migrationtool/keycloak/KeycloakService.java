e6be7eec4fa9a4424a78aa3885ae6db0d87d8ef2
tkuzynow <tomasz.kuzynowski@gmail.com>
2024-02-12 16:06:10 +0100

    fix: increase no of users to migrate at once

--- src/main/java/com/vi/migrationtool/keycloak/KeycloakService.java
+++ src/main/java/com/vi/migrationtool/keycloak/KeycloakService.java
@@ -35,7 +35,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class KeycloakService {
 
   private static final String SEARCH_PARAM = "search";
-  private static final String MAX_USERS_TO_MIGRATE = "500";
+  private static final String MAX_USERS_TO_MIGRATE = "850";
   private static final String PROVIDED_ROLE_DOESNT_EXISTS_IN_KEYCLOAK_MSG =
       "The provided role {} doesn't exists in keycloak, please create it first";
   private final KeycloakConfig keycloakConfig;
